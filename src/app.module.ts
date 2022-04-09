58224b59b02b17703e9ff45f5585b1d2494d5dd0
kimcodell <kimminhyuk0719@gmail.com>
2022-04-09 19:53:24 +0900

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: process.env.DB_PASSWORD,
+      database: 'play_diary_app',
+      entities: [],
+      synchronize: true, //TODO 데이터 저장 후에는 false로 수정 필수.
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
