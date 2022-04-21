868e41230f7412ef2a93effc07e788c9657b5a62
kimcodell <kimminhyuk0719@gmail.com>
2022-04-21 23:57:08 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('모바일 어플리케이션 API')
+    .setDescription('공연 정보 및 기록 모바일 어플리케이션 API')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const PORT = process.env.PORT;
+  await app.listen(PORT);
 }
 bootstrap();
