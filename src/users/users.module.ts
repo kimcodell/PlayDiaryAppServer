45a7af27fcba506b192fdce7d6d49cf660326eb4
minhyuk02 <minhyuk@indinity.co>
2022-05-20 15:44:57 +0900

    팔로우 API

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,16 @@
-import { UsersRepository } from './users.repository';
+import { UsersRepository } from '../repositories/users.repository';
 import { UserEntity } from './../models/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { FollowRepository } from 'src/repositories/follow.repository';
+import { FollowEntity } from 'src/models/follow.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, FollowEntity])],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository],
+  providers: [UsersService, UsersRepository, FollowRepository],
   exports: [UsersRepository],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { UsersRepository } from './users.repository';
+import { FollowRepository } from './../repositories/follow.repository';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { UsersRepository } from '../repositories/users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly followRepository: FollowRepository,
+  ) {}
 
   async getUserInfo(userId: Express.User) {
     return this.usersRepository.findUserInfoById(userId as number);
+  }
+
+  async followUser(followerId: number, followingId: number) {
+    const follower = await this.usersRepository.findSimpleUserById(followerId);
+    const following = await this.usersRepository.findSimpleUserById(
+      followingId,
+    );
+    if (!following) {
+      throw new HttpException(
+        '팔로우 하려는 사용자가 존재하지 않습니다.',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+    return await this.followRepository.follow(follower, following);
   }
 }
